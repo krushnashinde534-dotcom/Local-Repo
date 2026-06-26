4afde0248da08c6f355b84d88ffb223ba0e875df
krushna shinde <krushnashinde534@gmail.com>
2026-06-26 10:02:48 +0530

    Sample

---
+++ Sample.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std ;
+int main()
+{
+    int n;
+    cout <<"Enter a number:";
+    cin >>n;
+    for(int i = 1; i<=n; i++){
+        if(i %2 == 0) {
+            cout<<i<<endl;
+        }
+    }
+    cout<<"Thank you friends!";
+    return 0;
+}
