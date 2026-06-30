781f2482dd93c23be8004de33d054ba22c6348d8
krushna shinde <krushnashinde534@gmail.com>
2026-06-30 19:40:32 +0530

    Cube of N

---
+++ Cube.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int num, cube;
+    cout <<"Enter a number:";
+    cin >> num;
+    cube = num * num * num;
+    cout <<"Cube of " << num << "is" << cube;
+    return 0;
+}
