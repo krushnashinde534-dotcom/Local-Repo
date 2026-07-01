07c423632bf73cdd967b9583ad95bfece0606641
krushna shinde <krushnashinde534@gmail.com>
2026-07-01 17:55:44 +0530

    Sum & Product of Array

---
+++ Arr5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Function to calculate sum and product
+void sumAndProduct(int arr[], int size)
+{
+    int sum = 0;
+    int product = 1;
+
+    for(int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+        product *= arr[i];
+    }
+
+    cout << "Sum = " << sum << endl;
+    cout << "Product = " << product << endl;
+}
+
+int main()
+{
+    int arr[] = {2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    sumAndProduct(arr, size);
+
+    return 0;
+}
