e9243bb9210ce4fda2195199675d6da3b68ec317
krushna shinde <krushnashinde534@gmail.com>
2026-07-02 20:28:14 +0530

    Table of N

--- Table.cpp
+++ Table.cpp
@@ -1,13 +1,29 @@
+// # include <iostream>
+// using namespace std;
+// int main ()
+// {
+//     int n;
+//     cout <<"Enter the number:";
+//     cin >> n;
+//     cout <<"Table of "<<n<<endl;
+//     for (int i=1; i<=10; i++){
+//         cout << n <<"x" << i <<"="<< n*i <<endl;
+//     }
+//     return 0;
+// }
+
+
+
 # include <iostream>
 using namespace std;
-int main ()
-{
-    int n;
-    cout <<"Enter the number:";
-    cin >> n;
-    cout <<"Table of "<<n<<endl;
-    for (int i=1; i<=10; i++){
-        cout << n <<"x" << i <<"="<< n*i <<endl;
-    }
-    return 0;
+int main (){
+int n;
+cout<<"Enter a number:";
+cin>>n;
+for(int i = 1; i<=10; i++){
+    cout<<n*i<<endl;
+}
+cout<<"Table of "<<n<<endl;
+return 0;
+
 }
