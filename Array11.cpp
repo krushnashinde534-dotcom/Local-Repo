7bf93e22668a949df4f0c6cab23eea7c552253b3
krushna shinde <krushnashinde534@gmail.com>
2026-07-11 20:58:00 +0530

    Count even N in arr

--- Array11.cpp
+++ Array11.cpp
@@ -8,11 +8,11 @@ int main() {
     int count = 0;
 
     for(int i = 0; i < size; i++) {
-        if(arr[i] % 2 == 0)
-            count++;
-    }
-
-    cout << "Even Count = " << count;
+        if(arr[i] % 2 == 0);             
+         count++;
+    }   
+    
+    cout << "Even Count = " << count <<endl;   
 
     return 0;
 }
